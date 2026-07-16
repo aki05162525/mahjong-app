7371fa42dae0b259c4eaa4a9389f4a08c8b4ef26
aki05162525 <akihiro05162525@gmail.com>
2026-07-17 06:04:07 +0900

    fix: apply repository format to generated types

--- scripts/generate-database-types.sh
+++ scripts/generate-database-types.sh
@@ -7,5 +7,5 @@ formatted_file=$(mktemp)
 trap 'rm -f "$raw_file" "$formatted_file"' EXIT
 
 supabase gen types typescript --local > "$raw_file"
-prettier --parser typescript "$raw_file" > "$formatted_file"
+prettier --config .prettierrc --parser typescript "$raw_file" > "$formatted_file"
 mv "$formatted_file" src/lib/database.types.ts
